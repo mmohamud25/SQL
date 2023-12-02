59ec894a315eed99f17e777698c0bd056294f8db
Mohamed Mohamud <149164234+mmohamud25@users.noreply.github.com>
2023-12-02 10:18:51 -0500

    Create faculty.sql
    Faculty Table

---
+++ School_Database/faculty.sql
@@ -0,0 +1,8 @@
+CREATE TABLE faculty (
+    faculty_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    faculty_name TEXT,
+    faculty_position TEXT,
+    faculty_department TEXT,
+    faculty_email TEXT,
+    faculty_phone TEXT
+);

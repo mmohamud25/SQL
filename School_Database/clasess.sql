0f57c65cb5daff22c87e6a7ff66466212b8737ec
Mohamed Mohamud <149164234+mmohamud25@users.noreply.github.com>
2023-12-02 10:18:14 -0500

    Create clasess.sql
    Class Table

---
+++ School_Database/clasess.sql
@@ -0,0 +1,8 @@
+CREATE TABLE classes (
+    class_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    class_name TEXT,
+    class_teacher TEXT,
+    class_room TEXT,
+    class_schedule TEXT,
+    class_grade_level INTEGER
+);

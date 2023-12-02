e43df30449dfde40a14df8a10c35e86017343699
Mohamed Mohamud <149164234+mmohamud25@users.noreply.github.com>
2023-12-02 10:19:22 -0500

    Create staff.sql
    Staff Table

---
+++ School_Database/staff.sql
@@ -0,0 +1,8 @@
+CREATE TABLE staff (
+    staff_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    staff_name TEXT,
+    staff_role TEXT,
+    staff_department TEXT,
+    staff_email TEXT,
+    staff_phone TEXT
+);
